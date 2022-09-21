be61d9d4fdca8424039a285fa5620c74934c3f10
rodrigocamargo <rodrigocamargo854@gmail.com>
2022-09-21 19:12:44 -0300

    added image

--- src/pages/components/Banner/index.tsx
+++ src/pages/components/Banner/index.tsx
@@ -1,4 +1,4 @@
-import { Image, Box, Flex, Text } from "@chakra-ui/react";
+import { Image, Box, Flex, Text, Heading } from "@chakra-ui/react";
 
 export default function Banner() {
   return (
@@ -10,14 +10,36 @@ export default function Banner() {
       bgRepeat="no-repeat"
       bgSize="cover"
     >
-      <Flex 
-      justify={["center", "space-between"]} 
-      align="center" 
-      w="100%"
-      mx="auto"
-      px={["4","10","15","20","36"]}
+      <Flex
+        justify={["center", "space-between"]}
+        align="center"
+        w="100%"
+        mx="auto"
+        px={["4", "10", "15", "20", "36"]}
       >
-        <Text>5 Continentes, infinitas possibilidades.</Text>
+        <div>
+          <Heading
+            color="gray.100"
+            fontWeight="500"
+            fontSize={["xl", "2l", "2xl", "2xl", "4xl"]}
+          >
+            5 continentes,
+            <br /> infinitas possibildades
+          </Heading>
+          <Text
+            color="gray.300"
+            mt="5"
+            fontStyle={["0.8rem", "xl"]}
+            maxW={["100%", "100%", "100%", "550px"]}
+          >
+            {" "}
+            5 Continentes, infinitas possibilidades.
+          </Text>
+        </div>
+        <Image w={["300px", "300px", "300px", "430px"]} 
+        
+        display={["none","none",""]}
+        />
       </Flex>
     </Flex>
   );
